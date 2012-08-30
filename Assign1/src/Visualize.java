ccac56b8329603bd3a354989f55daa2fe80435a4
comp <aayush.iitdelhi@gmail.com>
2012-08-30 12:18:06 +0530

    have to redo changes

--- Assign1/src/Visualize.java
+++ Assign1/src/Visualize.java
@@ -16,13 +16,16 @@ import prefuse.Display;
 import prefuse.Visualization;
 import prefuse.action.ActionList;
 import prefuse.action.RepaintAction;
+import prefuse.action.assignment.ColorAction;
 import prefuse.action.assignment.SizeAction;
 import prefuse.action.assignment.DataColorAction;
 import prefuse.action.assignment.DataShapeAction;
+import prefuse.action.filter.GraphDistanceFilter;
 import prefuse.action.layout.graph.ForceDirectedLayout;
 import prefuse.activity.Activity;
 import prefuse.controls.DragControl;
 import prefuse.controls.FocusControl;
+import prefuse.controls.NeighborHighlightControl;
 import prefuse.controls.PanControl;
 import prefuse.controls.ZoomControl;
 import prefuse.controls.HoverActionControl;
@@ -30,11 +33,16 @@ import prefuse.data.Edge;
 import prefuse.data.Graph;
 import prefuse.data.Node;
 import prefuse.data.Table;
+import prefuse.data.Tuple;
+import prefuse.data.event.TupleSetListener;
 import prefuse.data.io.DataIOException;
+import prefuse.data.tuple.TupleSet;
 import prefuse.render.DefaultRendererFactory;
 import prefuse.render.EdgeRenderer;
+import prefuse.render.LabelRenderer;
 import prefuse.render.ShapeRenderer;
 import prefuse.util.ColorLib;
+import prefuse.util.ui.UILib;
 import prefuse.visual.VisualItem;
 
 
@@ -42,57 +50,82 @@ public class Visualize {
 	
 	//constructor
 	public Visualize(Graph graph){
-		//	Calculate all the edge types and other triad details
-	//	evaluate();
-			Visualization vis = new Visualization();
+		UILib.setPlatformLookAndFeel();
+			final Visualization vis = new Visualization();
 			vis.addGraph("graph",graph);
 			//vis.setInteractive("graph.edges",null,false);
 			//LabelRenderer r = new LabelRenderer("label");
 			//r.setRoundedCorner(8, 8);
 			ShapeRenderer s = new ShapeRenderer(8);
 			EdgeRenderer e = new EdgeRenderer(Constants.EDGE_TYPE_LINE);
-			vis.setRendererFactory(new DefaultRendererFactory(s,e));
+			LabelRenderer tr = new LabelRenderer();
+	        //tr.setRoundedCorner(1, 1);
+	        //tr.setMaxTextWidth(2);
+	        //m_vis.setRendererFactory(new DefaultRendererFactory(tr));
+	        DefaultRendererFactory defaultRenderer = new DefaultRendererFactory(s,e);
+	     //   defaultRenderer.add("label",tr);
+			vis.setRendererFactory(defaultRenderer);
+		//	vis.setRendererFactory(new DefaultRendererFactory(tr));
 	
-			ActionList color = new ActionList();
+
+				
 			//Different Colored nodes for different value fields
 			int[] palette = new int[]{ColorLib.rgb(180, 0, 0),ColorLib.rgb(0, 180, 0),ColorLib.rgb(0, 0, 180)};
 			//Different colored edges
 			int[] palette1 = new int[]{ColorLib.gray(180),ColorLib.gray(5)};
-	
+			
 			//	Different Shapes for different value fields
 			int[] shapes = new int[]{Constants.SHAPE_RECTANGLE,Constants.SHAPE_ELLIPSE,Constants.SHAPE_TRIANGLE_UP};
+			
+			//the following is used to separate the graph.nodes based on their values and colour them accordingly
 			DataColorAction fill = new DataColorAction("graph.nodes", "value", Constants.NOMINAL, VisualItem.FILLCOLOR, palette);
 			//ColorAction edges = new ColorAction("graph.edges",VisualItem.STROKECOLOR,ColorLib.gray(105));
 			//ColorAction text = new ColorAction("graph.nodes",VisualItem.TEXTCOLOR,ColorLib.gray(5));
 			DataShapeAction shape = new DataShapeAction("graph.nodes","value",shapes);
 			DataColorAction edgecolor = new DataColorAction("graph.edges","type",Constants.NOMINAL,VisualItem.STROKECOLOR,palette1);
+			 fill.add(VisualItem.FIXED, ColorLib.rgb(255,100,100));
+		     fill.add(VisualItem.HIGHLIGHT, ColorLib.rgb(255,200,125));
+			
+			//datacolour action is used to selectively add while colouraction is used to all
+			int hops = 30;
+	        final GraphDistanceFilter filter = new GraphDistanceFilter("graph", hops);
+	        
+			//action list binds all the data colour action , so we create actionlists and add to them
+			ActionList color = new ActionList();			
 			color.add(fill);
 			color.add(edgecolor);
 			//	color.add(text);
 			color.add(shape);
+			color.add(new RepaintAction());
+//			color.add(filter);
 			
 			ActionList layout = new ActionList(Activity.INFINITY);
+			//this actionlist will run indefinitely
 			//Force DIrected Layout
 			layout.add(new ForceDirectedLayout("graph"));
 			layout.add(new RepaintAction());
+			//layout.add(filter);
 	
 			SizeAction highl=new SizeAction("graph.nodes",1.0);
-			highl.add("_hover",2.0);
+			highl.add("_hover",3.0);
 			
 			ActionList highlight_neighbors=new ActionList();
 			highlight_neighbors.add(highl);
+			highlight_neighbors.add(filter);
 			vis.putAction("highlight_neighbors", highlight_neighbors);
 			vis.putAction("color",color);
 			vis.putAction("layout",layout);
 
-			Display d = new Display(vis);
+			Display d = new Display(vis); // so display is bigger than visualization
 			int width=720, height=480;
 			d.setBackground(Color.DARK_GRAY);
 			d.setSize(width,height);
 			d.pan(width/2.0, height/2.0);
 			d.addControlListener(new DragControl());
+			d.addControlListener(new FocusControl(1));
 			d.addControlListener(new PanControl());
 			d.addControlListener(new ZoomControl());
+			d.addControlListener(new NeighborHighlightControl());
 			//Action hoveracction=new Action();
 			//HoverActionControl hover=new HoverActionControl();
 			d.addControlListener(new HoverActionControl("highlight_neighbors"));
